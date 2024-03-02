43ee29e729c8fab1693e6a7b35354f3fba93065e
LimMengShin <62892957+LimMengShin@users.noreply.github.com>
2024-03-02 16:00:50 +0800

    Update 404 page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,7 +6,6 @@
 
 <CommonPage {title}>
     <div class="error-container">
-        <h1 class="error-code">404</h1>
         <p class="error-message">Oops! It looks like you're lost.</p>
         <p class="error-description">The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
         <a href="/" class="error-link">Go back home</a>
@@ -16,13 +15,6 @@
 <style lang="scss">
     .error-container {
         text-align: center;
-        margin-top: 50px;
-    }
-
-    .error-code {
-        font-size: 4rem;
-        margin-bottom: 10px;
-        color: #ff5722; /* Orange color */
     }
 
     .error-message {
@@ -32,22 +24,15 @@
 
     .error-description {
         font-size: 1rem;
-        margin-bottom: 20px;
-        color: #757575; /* Gray color */
+        margin-bottom: 50px;
     }
 
     .error-link {
         font-size: 1rem;
         padding: 10px 20px;
-        border: 2px solid #ff5722; /* Orange color */
+        border: 2px solid;
         border-radius: 4px;
-        color: #ff5722; /* Orange color */
         text-decoration: none;
         transition: background-color 0.3s ease-in-out, color 0.3s ease-in-out;
-    }
-
-    .error-link:hover {
-        background-color: #ff5722; /* Orange color */
-        color: #fff; /* White color */
     }
 </style>
